fb8996bdaf0e9fc584bfe58c116be4baf619ebbd
HarshitaKatariya <128654431+HarshitaKatariya@users.noreply.github.com>
2024-03-03 21:49:58 +0530

    add style.css and script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input"); 
+
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+    ul.appendChild(item);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    item.appendChild(span);
+    inp.value = "";
+
+})
+
+ul.addEventListener('click',function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('check');
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
+// let det = document.querySelector("span");
+// det.addEventListener('click',function(){
+//     let par = this.parentElement;
+//     console.log(par);
+// })
